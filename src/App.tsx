cc84ebf699bafb20cf1252ca8cbdde2d9bf247f3
Pietro Dessotti <pietrohdessotti@gmail.com>
2024-09-17 02:38:32 -0300

    add message to test

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import invitation from '/invite-marriege.pdf'
 function App() {
 
   return (
+    <>
+    'invitation' is a pdf file that is imported in the src folder
     <iframe style={{
       width: '100vw',
       height: '100vh',
       margin: '-10px',
     }} src={invitation}></iframe>
+    
+    </>
   )
 }
 

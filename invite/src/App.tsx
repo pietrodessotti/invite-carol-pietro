e0003f448465adc0025ad9a471c236ebd464b708
Pietro Dessotti <pietrohdessotti@gmail.com>
2024-09-17 02:21:20 -0300

    feat: project invite

---
+++ invite/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <iframe style={{
+      width: '100vw',
+      height: '100vh',
+      margin: '-10px'
+    }} src="/invite-marriege.pdf"></iframe>
+  )
+}
+
+export default App
